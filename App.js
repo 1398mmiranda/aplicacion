f65a020f9ceca65d98e6f7bc79201137ac9e9f16
sebastian <manolo_1398@outlook.es>
2022-05-16 10:38:44 -0500

    se agrega el proyecto

--- App.js
+++ App.js
@@ -1,20 +1,98 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  Alert,
+  Platform,
+} from "react-native";
+import { Picker } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import * as Sharing from "expo-sharing";
+import uploadToAnonymousFilesAsync from "anonymous-files";
 
-export default function App() {
+//import { useState } from "react/cjs/react.production.min";
+
+const App = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  let openImagePickerAsync = async () => {
+    let permissionResult =
+      await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("permission to access camera is required");
+      return;
+    }
+    const pickerResult = await ImagePicker.launchImageLibraryAsync();
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+    if (Platform.OS === 'web'){
+      const remoteUri=await uploadToAnonymousFilesAsync (pickerResult.uri)
+      setSelectedImage ({localUri: pickerResult.uri, remoteUri})
+
+    } else{
+      setSelectedImage({ localUri: pickerResult.uri });
+    }
+    
+
+
+  };
+  const openSharesDilog = async () => {
+    if (!(await Sharing.isAvailableAsync())) {
+      alert("the image is available for sharing at: ${selectedImage.remoteUri}");
+      return;
+    }
+    await Sharing.shareAsync(selectedImage.localUri);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}> Pick an Image!!</Text>
+      <TouchableOpacity onPress={openImagePickerAsync}>
+        <Image
+          source={{
+            uri:
+              selectedImage !== null
+                ? selectedImage.localUri
+                : "https://picsum.photos/200/300 ",
+          }}
+          style={styles.image}
+        />
+      </TouchableOpacity>
+      {selectedImage ? (
+        <TouchableOpacity style={styles.button} onPress={openSharesDilog}>
+          <Text style={styles.buttonText}> Shared this image</Text>
+        </TouchableOpacity>
+      ) : (
+        <View />
+      )}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#292929",
+  },
+  title: { fontSize: 30, color: "#fff" },
+  image: { width: 200, height: 200, borderRadius: 100, resizeMode: "contain" },
+  button: {
+    marginTop: 20,
+    backgroundColor: "deepskyblue",
+    padding: 7,
+  },
+
+  buttonText: {
+    color: "#fff",
+    fontSize: 20,
   },
 });
+
+export default App;
